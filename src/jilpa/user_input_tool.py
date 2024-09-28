35759ac588e75239fcd24a89072e7123d31cc164
kharish89 <k.harish89@gmail.com>
2024-09-28 16:30:28 -0700

    add user input tool

---
+++ src/jilpa/user_input_tool.py
@@ -0,0 +1,9 @@
+from crewai_tools.tools.base_tool import BaseTool
+
+class UserInputTool(BaseTool):
+    name: str = "UserInputTool"
+    description: str = "A tool to get input prompt from the user."
+
+def _run(self, **kwargs) -> str:
+    user_prompt = input("""Enter a scenario to extend: """)
+    return user_prompt
